3cf9263a21fab06de5bafd7c83ae38169fb50f37
Patrick Wong <Patrick Wong>
2015-07-27 14:29:47 -0700

    WARNING - HARDCODED VALUES RELATING TO HUE 3.7.0 AND SPARK 1.3.1
    MAPR-19692 - handle the json/marlin dev build

--- vm/script/cleanup.sh
+++ vm/script/cleanup.sh
@@ -16,3 +16,36 @@ rm -rf /tmp/*
 rm -rf /root/mapr-setup
 rm -rf /home/maprdev/*
 rm -rf /root/*.iso
+
+echo "AGGRESSIVE_DELETIONS=${AGGRESSIVE_DELETIONS}"
+if [ "x${AGGRESSIVE_DELETIONS}" = "xtrue" ]; then
+  echo "Cleaning very aggressiveley"
+  service mapr-warden stop
+  service mapr-zookeeper stop
+  
+  rm -rf /opt/mapr/hadoop/hadoop-2.*/share/hadoop/common/jdiff/*
+  rm -rf /opt/mapr/hadoop/hadoop-2.*/share/hadoop/common/sources/*
+  rm -rf /opt/mapr/hadoop/hadoop-2.*/share/hadoop/hdfs/jdiff/*
+  rm -rf /opt/mapr/hadoop/hadoop-2.*/share/hadoop/hdfs/sources/*
+  rm -rf /opt/mapr/hadoop/hadoop-2.*/share/hadoop/httpfs/*
+  rm -rf /opt/mapr/hadoop/hadoop-2.*/share/hadoop/mapreduce/*
+  rm -rf /opt/mapr/hadoop/hadoop-2.*/share/hadoop/yarn/sources/*
+  rm -rf /opt/mapr/hadoop/hadoop-2.*/share/hadoop/yarn/test/*
+  rm -rf /opt/mapr/hadoop/hadoop-2.*/share/hadoop/tools/*
+  
+  rm -rf /opt/mapr/hadoop/hadoop-0.20.2/lib/jdiff/*
+  
+  rm -rf /opt/mapr/logs/*
+  
+  rm -f /opt/mapr/server/logdump
+  rm -f /opt/mapr/server/fsck
+  rm -f /opt/mapr/server/fsck-phase6
+  rm -f /opt/mapr/server/
+  rm -f /opt/mapr/server/
+  
+  rm -rf /opt/mapr/server/tools
+  rm -rf /opt/mapr/support
+  
+  find /opt/mapr/server/ -type f -exec file -i '{}' \; | grep 'application/x-' | sed 's/:.*//g' | xargs strip
+  find /opt/mapr/lib/ -type f -exec file -i '{}' \; | grep 'application/x-' | sed 's/:.*//g' | xargs strip
+fi
