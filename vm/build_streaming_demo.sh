85941666bc516769c9984ac14cda754a00f8ed30
dschexna <dschexna@mapr.com>
2016-06-22 13:32:02 -0400

    Script to build the streaming demo

--- vm/build_streaming_demo.sh
+++ vm/build_streaming_demo.sh
@@ -20,7 +20,7 @@ SBPARAMS="
 --var mapr_core_repo_url=http://package.mapr.com/releases
 --var mapr_eco_repo_url=http://package.mapr.com/releases/ecosystem-5.x
 --var mapr_banner_url=http://%s:8443/
---var mapr_banner_name=MapR-Sandbox-Streaming-Demo
+--var mapr_banner_name=MapR-Streaming-Demo
 mapr-streaming-demo.json
 "
 
